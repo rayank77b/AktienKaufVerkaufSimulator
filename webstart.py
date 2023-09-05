f23f83a6ca292cc20618c427cdefd57912524651
ray <ray@azul>
2023-09-05 14:57:07 +0200

    add stocks tickers table list

--- webstart.py
+++ webstart.py
@@ -1,4 +1,9 @@
-from flask import Flask  
+from flask import Flask
+from flask import render_template
+import sqlite3
+
+db_name = 'db/Stocks.DB.sql3'
+
 
 WE_DEBUG = True
 
@@ -10,7 +15,14 @@ def root_index():
 
 @app.route('/stocks') 
 def stocks_index():
-    return "here we start";  
+    conn = sqlite3.connect(db_name)
+    cursor = conn.execute("SELECT ISIN, Name from Tickers")
+    stocks = {}
+    for row in cursor:
+        stocks[row[0]] = row[1]
+    conn.close()
+    return render_template('stocks.html', stocks = stocks)
+
     
-if __name__ =='__main__':  
+if __name__ =='__main__':
     app.run(debug = WE_DEBUG)    
