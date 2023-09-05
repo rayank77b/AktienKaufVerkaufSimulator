11707aedf67bbfa0b194d522a0c7dde0fd2071e0
ray <ray@azul>
2023-09-05 15:48:02 +0200

    add price info for a specific isin name

--- webstart.py
+++ webstart.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 import sqlite3
+import yfinance as yf
 
 db_name = 'db/Stocks.DB.sql3'
 
@@ -23,6 +24,20 @@ def stocks_index():
     conn.close()
     return render_template('stocks.html', stocks = stocks)
 
+@app.route('/isin/<isin>') 
+def isin_name_info(isin):
+    data = {}
+    conn = sqlite3.connect(db_name)
+    sql = "SELECT ISIN, Name from Tickers WHERE ISIN ='%s'"%isin
+    cursor = conn.execute(sql)
+    data['isin'] = isin
+    for row in cursor:
+        data['name'] = row[1]
+    conn.close()
+    stock = yf.Ticker(isin)
+    price = stock.info['currentPrice']
+    data['price'] = price
+    return render_template('isin.html', data = data)
     
 if __name__ =='__main__':
     app.run(debug = WE_DEBUG)    
